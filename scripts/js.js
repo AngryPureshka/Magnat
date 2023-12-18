4f138d7432248f03ba3bf23fae5fd082a8f15df2
AngryPureshka <77552669+AngryPureshka@users.noreply.github.com>
2023-12-18 13:49:25 +0500

    Add files via upload

--- scripts/js.js
+++ scripts/js.js
@@ -1,21 +1,34 @@
 document.querySelector("main").style.paddingTop = document.querySelector("header").offsetHeight + "px";
 
 if(window.innerWidth < 591){
+
+    let nav = document.querySelector("header").querySelector("nav");
+
     document.querySelector(".header-burger").addEventListener("click", function(){
-        if (document.querySelector(".header-nav").style.display == "none"){
-            document.querySelector(".header-nav").style.display = "block";
-        } else {
+        if (document.querySelector(".header-nav").style.display == "block"){
             document.querySelector(".header-nav").style.display = "none";
+            nav.style.opacity = 0;
+        } else {
+            document.querySelector(".header-nav").style.display = "block";
+            nav.style.opacity = 1;
         }
     })
-    
-    let nav = document.querySelector("header").querySelector("nav");
     
     document.querySelector("body").addEventListener("click", function(e){
         if(e.target == document.querySelector(".header-burger") || e.target == nav){
             return;
         } else {
             document.querySelector(".header-nav").style.display = "none";
+            nav.style.opacity = 0;
+            document.querySelector(".cost").style.display = "none";
         }
     })
 }
+
+document.querySelector(".banner-btn").addEventListener("click", function(){
+    document.querySelector(".cost").style.display = "block";
+})
+
+document.querySelector(".close").addEventListener("click", function(e){
+    document.querySelector(".cost").style.display = "none";
+})
